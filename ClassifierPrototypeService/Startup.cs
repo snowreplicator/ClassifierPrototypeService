0ff248e5ccca44d75c624ecbcdefadec9223edd6
SnowReplicator <snowrepl@gmail.com>
2024-03-09 16:03:21 +0300

    add cors for any origin

--- ClassifierPrototypeService/Startup.cs
+++ ClassifierPrototypeService/Startup.cs
@@ -14,22 +14,36 @@ namespace Prototype.ClassifierPrototypeService;
 public class Startup
 {
     private IConfiguration Configuration { get; }
-    
+
     public Startup(IConfiguration configuration)
     {
         Configuration = configuration;
     }
-    
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.Configure<ServiceOptions>(Configuration);
         ServiceOptions serviceOptions = Configuration.Get<ServiceOptions>();
-        
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("ClassifierPrototypePolicy", builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+
         services.AddControllers().AddNewtonsoftJson(JsonConfiguration.ConfigureJsonSerializer);
 
-        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Classifier prototype", Version = "v1" }); });
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo {Title = "Classifier prototype", Version = "v1"});
+        });
         services.AddClassifierPrototypeSwagger();
-        
+
         services.AddInfrastructure(Configuration);
         services.AddApplication();
     }
@@ -44,8 +58,10 @@ public class Startup
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Classifier prototype rest api"));
         }
 
+        app.UseCors("ClassifierPrototypePolicy");
+
         app.UseRouting();
-        
+
         app.UseToBaseExceptionWrapping();
 
         app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
