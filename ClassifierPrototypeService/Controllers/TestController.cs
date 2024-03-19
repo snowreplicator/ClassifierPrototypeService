63e6fd51a2384175486fb586e9bbcb062e2c5adb
SnowReplicator <snowrepl@gmail.com>
2024-03-19 08:35:12 +0300

    enviroment test in controller

--- ClassifierPrototypeService/Controllers/TestController.cs
+++ ClassifierPrototypeService/Controllers/TestController.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Prototype.ClassifierPrototypeService.Application.ApplicationServices.Interfaces.Commands;
 using Prototype.ClassifierPrototypeService.Application.RequestModels;
@@ -8,11 +8,17 @@ namespace Prototype.ClassifierPrototypeService.Controllers;
 
 public class TestController : BaseController
 {
+    private readonly IWebHostEnvironment _env;
+
+    public TestController(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+    
     /// <summary>
     /// test1
     /// </summary>
     [HttpGet("Test1")]
-    [Description("test 1")]
     public async Task<EmptyResult> Test1Async()
     {
         await GetApplicationService<ITest1ApplicationService>().HandleAsync(new EmptyRequest());
@@ -23,10 +29,24 @@ public class TestController : BaseController
     /// test2
     /// </summary>
     [HttpGet("Test2")]
-    [Description("test 2")]
     public async Task<EmptyResult> Test2Async()
     {
         await GetApplicationService<ITest2ApplicationService>().HandleAsync(new EmptyRequest());
         return new EmptyResult();
     }
+    
+    /// <summary>
+    /// test3
+    /// </summary>
+    [HttpGet("Test3")]
+    public object Test3()
+    {
+        object obj = new
+        {
+            environment = _env.EnvironmentName 
+        };
+        
+        return new ActionResult<object>(obj);
+    }
+
 }
